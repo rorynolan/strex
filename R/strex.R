ff4f7844b8007d289af8e70ae51b1b5a6a6063b8
Rory Nolan <rorynoolan@gmail.com>
2018-08-31 00:26:07 +0100

    Fix another typo.

--- R/strex.R
+++ R/strex.R
@@ -17,8 +17,8 @@ if (getRversion() >= "2.15.1") {
 #'
 #' There are some things that I wish were easier with the `stringr` or `stringi`
 #' packages. The foremost of these is the extraction of numbers from strings.
-#' `stringr` lets you figure this out the regex for yourself; `strex` takes care
-#' of this for you. There are many more useful functionalities in `strex`. In
+#' `stringr` makes you figure out the regex for yourself; `strex` takes care of
+#' this for you. There are many more useful functionalities in `strex`. In
 #' particular, there's a `match_arg()` function which is more flexible than the
 #' base `match.arg()`. Contributions to this package are encouraged: it is
 #' intended as a miscellany of string manipulation functions which cannot be
--- index.md
+++ index.md
@@ -5,7 +5,7 @@
 
 There are some things that I wish were easier with the `stringr` or
 `stringi` packages. The foremost of these is the extraction of numbers
-from strings. `stringr` lets you figure this out the regex for yourself;
+from strings. `stringr` makes you figure out the regex for yourself;
 `strex` takes care of this for you. There are many more useful
 functionalities in `strex`. In particular, there’s a `match_arg()`
 function which is more flexible than the base `match.arg()`.
