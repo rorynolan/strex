9c634c5d65e9b566802d03e4e26d3b0c69df7854
Rory Nolan <rorynoolan@gmail.com>
2019-06-14 16:11:49 +0100

    Allow for configure being run twice.

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -13,17 +13,19 @@ gcc_version <- function() {
 
 replace_R_fun <- function(orig_lines, fun_name, new_fun_body) {
   fun_pattern <- stringr::fixed(paste0(fun_name, " <- function("))
-  fun_def_start_line <- match(TRUE,
-                              stringr::str_detect(orig_lines, fun_pattern))
+  fun_def_start_line <- stringr::str_which(orig_lines, fun_pattern)[[1]]
+  if (Sys.getenv("TRAVIS") == "true" && is.na(fun_def_start_line)) {
+    cat(paste0("Didn't find function ", fun_name, ".\n"))
+    return(invisible(FALSE))
+  }
   fun_def_end_line <- fun_def_start_line +
-    match(TRUE,
-          stringr::str_detect(orig_lines[-seq_len(fun_def_start_line)],
-                              "^\\}\\s*$"))
+    stringr::str_which(orig_lines[-seq_len(fun_def_start_line)], "^\\}\\s*$")
   c(orig_lines[seq_len(fun_def_start_line)],
     new_fun_body,
     orig_lines[seq(fun_def_end_line, length(orig_lines))])
 }
 file_replace_R_fun <- function(path, fun_name, new_fun_body) {
+  if (Sys.getenv("TRAVIS") == "true") cat(paste0("File: ", path, ".\n"))
   orig_lines <- readLines(path)
   new_lines <- replace_R_fun(orig_lines, fun_name, new_fun_body)
   writeLines(new_lines, path)
@@ -35,10 +37,12 @@ file_replace_R_funs <- function(path, fun_names, new_fun_bodies) {
 }
 
 remove_C_fun <- function(orig_lines, fun_sig) {
-  fun_def_start_line <- match(
-    TRUE,
-    stringr::str_detect(orig_lines, stringr::fixed(fun_sig))
-  )
+  fun_def_start_line <- stringr::str_which(orig_lines,
+                                           stringr::fixed(fun_sig))[[1]]
+  if (Sys.getenv("TRAVIS") == "true" && is.na(fun_def_start_line)) {
+    cat(paste0("Didn't find function ", fun_sig, ".\n"))
+    return(invisible(FALSE))
+  }
   while (fun_def_start_line > 1) {
     if (startsWith(orig_lines[fun_def_start_line - 1], "//")) {
       fun_def_start_line <- fun_def_start_line - 1
@@ -53,6 +57,7 @@ remove_C_fun <- function(orig_lines, fun_sig) {
   c(orig_lines[-seq(fun_def_start_line, fun_def_end_line)])
 }
 file_remove_C_fun <- function(path, fun_sig) {
+  if (Sys.getenv("TRAVIS") == "true") cat(paste0("File: ", path, ".\n"))
   orig_lines <- readLines(path)
   new_lines <- remove_C_fun(orig_lines, fun_sig)
   writeLines(new_lines, path)
@@ -71,7 +76,11 @@ remove_matching_lines <- function(orig_lines, patterns) {
     }
   )
   matching_lines <- unique(unlist(matching_lines))
-  orig_lines[-matching_lines]
+  if (length(matching_lines)) {
+    orig_lines[-matching_lines]
+  } else {
+    orig_lines
+  }
 }
 file_remove_matching_lines <- function(path, patterns) {
   orig_lines <- readLines(path)
@@ -86,8 +95,6 @@ if (!is.na(gcc_version()) && gcc_version() < "4.9") {
   file_replace_R_fun("R/RcppExports.R", "lst_char_to_num",
                      c("  if (commas) x %<>% lapply(str_replace_all, ',', '')",
                        "  lapply(x, as.numeric)"))
-  cat("Printing list-utils.cpp.\n")
-  cat(readLines("src/list-utils.cpp"), sep = "\n")
   cat("Removing C fun.\n")
   file_remove_C_fun("src/list-utils.cpp",
                     "List lst_char_to_num(List x, bool commas)")
